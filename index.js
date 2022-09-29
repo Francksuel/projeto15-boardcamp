8509cb3dd7b4e319ee454359b24fafe17983e8b4
Francksuel <francksuelbarbosa@gmail.com>
2022-09-29 13:21:39 -0300

    Post categories

--- index.js
+++ index.js
@@ -14,9 +14,34 @@ const connection = new Pool({
 	connectionString: process.env.DATABASE_URL,
 });
 
-app.get("/categories", async(req, res) => {
-    const category = await connection.query('SELECT * FROM categories;');    
+app.get("/categories", async (req, res) => {
+	const category = await connection.query("SELECT * FROM categories;");
 	res.send(category.rows);
+});
+
+app.post("/categories", async (req, res) => {
+	const { name } = req.body;
+	if (!name) {
+		return res.sendStatus(400);
+	}
+
+	try {
+		const categories = await connection.query("SELECT * FROM categories;");
+		console.log(categories.rows);
+		const isRepeatName = categories.rows.filter(
+			(element) => element.name === name
+		);
+		if (isRepeatName.length !== 0) {
+			return res.sendStatus(409);
+		}
+		const response = await connection.query(
+			"INSERT INTO categories (name) VALUES ($1);",
+			[name]
+		);
+		res.send(response);
+	} catch {
+		res.sendStatus(500);
+	}
 });
 
 app.listen(4000, () => {
