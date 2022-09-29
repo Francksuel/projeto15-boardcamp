13bbac1aff285a1998b3d967882b223eed7c9480
Francksuel <francksuelbarbosa@gmail.com>
2022-09-29 11:35:42 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import pg from "pg";
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const { Pool } = pg;
+const connection = new Pool({
+	connectionString: process.env.DATABASE_URL,
+});
+
+app.get("/", async(req, res) => {
+    const category = await connection.query('SELECT * FROM categories;');
+    
+	res.send(category.rows);
+});
+
+app.listen(4000, () => {
+	console.log("Listening on port 4000");
+});
