f76e28a97af3c61eb41f88fb550afb9c4c8938bf
Francksuel <francksuelbarbosa@gmail.com>
2022-10-01 19:42:48 -0300

    Editing a customer

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -22,16 +22,18 @@ const getCustomers = async (req, res) => {
 };
 
 const getCustomer = async (req, res) => {
-	const id = req.params.id;	
+	const id = req.params.id;
 	try {
 		const customer = await connection.query(
 			`SELECT * FROM customers WHERE customers.id = $1;`,
 			[id]
-		);			
+		);
 		if (customer.rows.length === 0) {
 			return res.sendStatus(404);
 		}
-		customer.rows[0].birthday = customer.rows[0].birthday.toISOString().substring(0, 10);
+		customer.rows[0].birthday = customer.rows[0].birthday
+			.toISOString()
+			.substring(0, 10);
 		res.send(customer.rows[0]);
 	} catch {
 		res.sendStatus(500);
@@ -57,4 +59,38 @@ const addCustomer = async (req, res) => {
 		res.sendStatus(500);
 	}
 };
-export { getCustomers, addCustomer, getCustomer };
+
+const editCustomer = async (req, res) => {
+	const customer = res.locals.customer;
+	const id = req.params.id;
+	try {
+		const customerEdited = await connection.query(
+			`SELECT * FROM customers WHERE customers.id = $1;`,
+			[id]
+		);
+		if (customerEdited.rows.length === 0) {
+			return res.sendStatus(404);
+		}
+		const customers = await connection.query("SELECT * FROM customers;");
+		const isRepeatCpf = customers.rows.filter(
+			(element) => element.cpf === customer.cpf
+		);
+		if (isRepeatCpf.length > 0) {
+			if (isRepeatCpf.length > 1) {
+				return res.sendStatus(409);
+			}
+			if (isRepeatCpf[0].id !== Number(id)) {
+				return res.sendStatus(409);
+			}
+		}
+		await connection.query(
+			"UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id = $5;",
+			[customer.name, customer.phone, customer.cpf, customer.birthday, id]
+		);
+		res.sendStatus(200);
+	} catch {
+		res.sendStatus(500);
+	}
+};
+
+export { getCustomers, addCustomer, getCustomer, editCustomer };
--- src/routers/customers.routers.js
+++ src/routers/customers.routers.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { addCustomer, getCustomer, getCustomers } from "../controllers/customers.controllers.js";
+import { addCustomer, editCustomer, getCustomer, getCustomers } from "../controllers/customers.controllers.js";
 import { customerValidationSchema } from "../middlewares/customer.middleware.js";
 
 const customersRouter = express.Router();
 
 customersRouter.get("/customers", getCustomers);
 customersRouter.get("/customers/:id", getCustomer);
-customersRouter.post("/customers",customerValidationSchema, addCustomer);;
+customersRouter.post("/customers",customerValidationSchema, addCustomer);
+customersRouter.put("/customers/:id",customerValidationSchema, editCustomer);
 
 export { customersRouter };
